0f9fc2976eaa06ca157a374ca54e52a079304b5e
Hayeon LEE <95855640+cocorig@users.noreply.github.com>
2023-06-27 19:16:46 +0900

    Fetch movie data from API and update state

--- movie_app_ver1/src/App.js
+++ movie_app_ver1/src/App.js
@@ -1,53 +1,17 @@
-import React, { Component } from 'react';
-import Movie from './Movie';
-import axios from 'axios';
-import './App.css';
+import React from "react";
+import {HashRouter, Route} from 'react-router-dom';
+import About from './routes/About'
+import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
+function App(){
+  return (
+  <HashRouter>
+    <Navigation/>
+    <Route  path="/about" exact={true} component={About}/>
+    <Route path="/" exact={true} component={Home}/>
+  </HashRouter>
+  )
+}
 
-class App extends Component {
-  
-  state = {
-    isLoading : true,
-    movies : [],
-  }
 
-  getMovies = async ()=>{ //너는 이걸 기다려야해
-    const {data:{data:{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    this.setState({movies , isLoading : false});  
-    console.log(this.state)
-  }
-  componentDidMount(){ //컴포넌트가 처음 렌더링될때 호출됨,api로 부터 data가 feching이 완료되면 
-   this.getMovies();
-  
-  }
-  render() {
-    const {isLoading , movies} = this.state;
-    return (
-      <section className = "container">
-        {isLoading ?   (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ):(
-
-          <div className='movies'>
-              {movies.map(movies=>(
-                <Movie key={movies.id} 
-                id= {movies.id}
-                year={movies.year}
-                title={movies.title} 
-                summary={movies.summary}
-                poster={movies.medium_cover_image}
-                genres={movies.genres}
-            />
-            ))}
-          </div>
-          )}
-
-      </section>
-        );
-      }
-    }
 export default App;
-
-//this.state.isLoading => const { isLoading } = this.state;
-//async await을 하는 것은 기본적으로 js에게 getMovies함수에게 조금 시간이 필요하고 그걸 기다려야한다고 말하는것/
